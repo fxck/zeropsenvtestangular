a2c5d2556841981cc95197183f0d78a9b937f024
Ales Rechtorik <foxxcz@gmail.com>
2022-04-06 14:27:49 +0200

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'zeropsenvtest';
+  constructor() {
+    console.log({ raw: environment.recipeConfig, parsed: JSON.parse(environment.recipeConfig) });
+  }
 }
